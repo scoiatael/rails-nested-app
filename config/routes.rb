3d2e0f9e266103768ab26a6b08f39937e1723224
LC <eternal.kadir@gmail.com>
2014-11-01 17:43:06 +0000

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :questions
 
   resources :questionnaires
+  root 'questionnaires#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
